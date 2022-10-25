2a731e59df3c1ab4790ca19ba6044d2ec309c9ac
Irene Silva França <105497075+issf69@users.noreply.github.com>
2022-10-25 18:55:27 -0300

    Add files via upload

---
+++ MinhaClasse.java
@@ -0,0 +1,21 @@
+package irene.silva.segundasemana;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+
+        String primeiroNome = "Irene";
+        String segundoNome = "Silva";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+
+}
